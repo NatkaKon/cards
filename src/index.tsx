febca408f62cc8032086aa0628b11b5d5cb90699
Natalia Kondratieva <kondratieva07@gmail.com>
2023-03-03 20:13:01 +0300

    add provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {store} from './app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
