be15d165fdfe3179f4f5dd86c0c9a413027b994b
Natalia Kondratieva <kondratieva07@gmail.com>
2023-03-04 13:46:43 +0300

    feat(API): create instance

---
+++ src/app/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+  withCredentials: true,
+})
+
+export const authAPI = {}
