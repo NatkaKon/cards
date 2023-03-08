771d2bc0d4c649af80e662aebe2a2483bc1dd8ea
m1rade <forx98@gmail.com>
2023-03-09 00:02:44 +0300

    feat(Signup): added signupReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import thunk from 'redux-thunk'
 
+import { signupReducer } from '../features/Registration/sign-up-reducer'
+
 import { appReducer } from './app-reducer'
 
 const rootReducer = combineReducers({
   appReducer: appReducer,
+  signup: signupReducer,
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
