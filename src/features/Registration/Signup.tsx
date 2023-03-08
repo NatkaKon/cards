736a2a8cb3fc5b7db2aca7e5b37521c5ac964a8d
m1rade <forx98@gmail.com>
2023-03-09 02:39:24 +0300

    feat(signup): changed style

--- src/features/Registration/Signup.tsx
+++ src/features/Registration/Signup.tsx
@@ -5,7 +5,6 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 import Button from '@mui/material/Button'
 import FormControl from '@mui/material/FormControl'
 import FormGroup from '@mui/material/FormGroup'
-import FormLabel from '@mui/material/FormLabel'
 import IconButton from '@mui/material/IconButton'
 import InputAdornment from '@mui/material/InputAdornment'
 import Paper from '@mui/material/Paper'
@@ -24,10 +23,10 @@ export const Signup: FC = () => {
   // const isRegistered = useAppSelector<AppRootStateType>(state => state.signup.isRegistered)
   //const dispatch = useAppDispatch();
 
-  const paperFormStyle = { p: '33px', minWidth: '413px' }
+  const paperFormStyle = { py: '20px', px: '30px', minWidth: '413px' }
   const buttonStyle = {
     borderRadius: '30px',
-    mt: '60px',
+    mt: '20px',
     boxShadow: 6,
   }
 
@@ -64,93 +63,92 @@ export const Signup: FC = () => {
   // if (isRegistered) return <Navigate to={PATH.LOGIN} />
 
   return (
-    <div className={style.formContainer}>
-      <Paper elevation={4} sx={paperFormStyle}>
+    <div className={style.signupBlock}>
+      <div className={style.formContainer}>
         <form onSubmit={formik.handleSubmit}>
-          <FormControl fullWidth margin="dense">
-            <FormLabel>
-              <h1>Регистрация</h1>
-            </FormLabel>
-            <FormGroup>
-              <TextField
-                id="email"
-                type="text"
-                variant="standard"
-                label="Email"
-                margin="dense"
-                {...formik.getFieldProps('email')}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email ? formik.errors.email : ' '}
-              />
-              <TextField
-                id="password"
-                type={showPassword ? 'text' : 'password'}
-                variant="standard"
-                label="Пароль"
-                margin="dense"
-                {...formik.getFieldProps('password')}
-                error={formik.touched.password && Boolean(formik.errors.password)}
-                helperText={
-                  formik.touched.password && formik.errors.password ? formik.errors.password : ' '
+          <Paper elevation={6} sx={paperFormStyle}>
+            <FormControl fullWidth margin="dense">
+              <div className={style.formHeader}>Регистрация</div>
+              <FormGroup>
+                <TextField
+                  id="email"
+                  type="text"
+                  variant="standard"
+                  label="Email"
+                  {...formik.getFieldProps('email')}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={
+                    formik.touched.email && formik.errors.email ? formik.errors.email : ' '
+                  }
+                />
+                <TextField
+                  id="password"
+                  type={showPassword ? 'text' : 'password'}
+                  variant="standard"
+                  label="Пароль"
+                  {...formik.getFieldProps('password')}
+                  error={formik.touched.password && Boolean(formik.errors.password)}
+                  helperText={
+                    formik.touched.password && formik.errors.password ? formik.errors.password : ' '
+                  }
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowPassword}>
+                          {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextField
+                  id="confirmPassword"
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  variant="standard"
+                  label="Повторите пароль"
+                  {...formik.getFieldProps('confirmPassword')}
+                  error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                  helperText={
+                    formik.touched.confirmPassword && formik.errors.confirmPassword
+                      ? formik.errors.confirmPassword
+                      : ' '
+                  }
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowConfirmPassword}>
+                          {showConfirmPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  size="medium"
+                  disabled={formik.isSubmitting}
+                  sx={buttonStyle}
+                >
+                  Зарегистрироваться
+                </Button>
+              </FormGroup>
+            </FormControl>
+            <div className={style.formFooter}>
+              <div className={style.formEndnote}>Уже есть аккаунт?</div>
+              <NavLink
+                to={PATH.LOGIN}
+                className={({ isActive }) =>
+                  isActive ? style.activeLink + style.loginLink : style.loginLink
                 }
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleClickShowPassword}>
-                        {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <TextField
-                id="confirmPassword"
-                type={showConfirmPassword ? 'text' : 'password'}
-                variant="standard"
-                label="Повторите пароль"
-                margin="dense"
-                {...formik.getFieldProps('confirmPassword')}
-                error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
-                helperText={
-                  formik.touched.confirmPassword && formik.errors.confirmPassword
-                    ? formik.errors.confirmPassword
-                    : ' '
-                }
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleClickShowConfirmPassword}>
-                        {showConfirmPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Button
-                type="submit"
-                color="primary"
-                variant="contained"
-                size="medium"
-                disabled={formik.isSubmitting}
-                sx={buttonStyle}
               >
-                Зарегистрироваться
-              </Button>
-            </FormGroup>
-          </FormControl>
+                Войти
+              </NavLink>
+            </div>
+          </Paper>
         </form>
-        <div className={style.formFooter}>
-          <div>Уже есть аккаунт?</div>
-          <NavLink
-            to={PATH.LOGIN}
-            className={({ isActive }) =>
-              isActive ? style.activeLink + style.loginLink : style.loginLink
-            }
-          >
-            Войти
-          </NavLink>
-        </div>
-      </Paper>
+      </div>
     </div>
   )
 }
