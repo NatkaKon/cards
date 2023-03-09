27494ef48c5ca10fd9f2397b7a769e9a8bec0be4
m1rade <forx98@gmail.com>
2023-03-09 20:22:31 +0300

    feat(PasswordRecovery): added forgot password page

--- src/features/PasswordRecovery/PasswordRecovery.tsx
+++ src/features/PasswordRecovery/PasswordRecovery.tsx
@@ -1,5 +1,92 @@
 import { FC } from 'react'
 
+import { FormLabel } from '@mui/material'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import FormGroup from '@mui/material/FormGroup'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import { useFormik } from 'formik'
+import { NavLink } from 'react-router-dom'
+import * as Yup from 'yup'
+
+import { PATH } from '../../common/constants/path'
+
+import style from './PasswordRecovery.module.css'
+
 export const PasswordRecovery: FC = () => {
-  return <div>Password Recovery</div>
+  const buttonStyle = {
+    borderRadius: '30px',
+    mt: '20px',
+    boxShadow: 6,
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+
+    onSubmit: (values: PasswordRecoveryFormType) => {
+      alert(JSON.stringify(values))
+    },
+  })
+
+  return (
+    <div className={style.formBlock}>
+      <div className={style.formContainer}>
+        <form onSubmit={formik.handleSubmit}>
+          <Paper elevation={6} sx={{ py: '40px', px: '30px', maxWidth: '413px' }}>
+            <FormControl fullWidth>
+              <div className={style.formHeader}>Forgot your password?</div>
+              <FormGroup>
+                <TextField
+                  id="email"
+                  type="text"
+                  variant="standard"
+                  label="Email"
+                  {...formik.getFieldProps('email')}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={
+                    formik.touched.email && formik.errors.email ? formik.errors.email : ' '
+                  }
+                />
+                <FormLabel>
+                  <p>Enter your email address and we will send you further instructions</p>
+                </FormLabel>
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  size="medium"
+                  sx={buttonStyle}
+                >
+                  Send Instructions
+                </Button>
+              </FormGroup>
+              <div className={style.formFooter}>
+                <div className={style.formEndnote}>Did you remember your password?</div>
+                <NavLink
+                  to={PATH.LOGIN}
+                  className={({ isActive }) =>
+                    isActive ? style.activeLink + style.loginLink : style.loginLink
+                  }
+                >
+                  Try logging in
+                </NavLink>
+              </div>
+            </FormControl>
+          </Paper>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+//types
+type PasswordRecoveryFormType = {
+  email: string
 }
