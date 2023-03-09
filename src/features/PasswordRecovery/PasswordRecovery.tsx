e1a40aa1b8adb48b3153927ab12b326d418ee670
m1rade <forx98@gmail.com>
2023-03-09 21:57:46 +0300

    feat(PasswordRecovery): added check email page

--- src/features/PasswordRecovery/PasswordRecovery.tsx
+++ src/features/PasswordRecovery/PasswordRecovery.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, memo, useState, MouseEvent } from 'react'
 
 import { FormLabel } from '@mui/material'
 import Button from '@mui/material/Button'
@@ -7,14 +7,20 @@ import FormGroup from '@mui/material/FormGroup'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 import { useFormik } from 'formik'
-import { NavLink } from 'react-router-dom'
+import { NavLink, redirect } from 'react-router-dom'
 import * as Yup from 'yup'
 
 import { buttonStyle } from '../../common/constants/form-button-style'
 import { PATH } from '../../common/constants/path'
 import style from '../../common/styles/authForm.module.css'
 
-export const PasswordRecovery: FC = () => {
+import { ReactComponent as EmailPicture } from './email-image.svg'
+
+const paperStyle = { py: '40px', px: '30px', maxWidth: '413px' }
+
+export const PasswordRecovery: FC = memo(() => {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -25,60 +31,99 @@ export const PasswordRecovery: FC = () => {
     }),
 
     onSubmit: (values: PasswordRecoveryFormType) => {
-      alert(JSON.stringify(values))
+      setIsSubmitted(true)
     },
   })
 
   return (
+    <>
+      {isSubmitted ? (
+        <CheckEmail email={formik.values.email} />
+      ) : (
+        <div className={style.formBlock}>
+          <div className={style.formContainer}>
+            <form onSubmit={formik.handleSubmit}>
+              <Paper elevation={6} sx={paperStyle}>
+                <FormControl fullWidth>
+                  <div className={style.formHeader}>Forgot your password?</div>
+                  <FormGroup>
+                    <TextField
+                      id="email"
+                      type="text"
+                      variant="standard"
+                      label="Email"
+                      {...formik.getFieldProps('email')}
+                      error={formik.touched.email && Boolean(formik.errors.email)}
+                      helperText={
+                        formik.touched.email && formik.errors.email ? formik.errors.email : ' '
+                      }
+                    />
+                    <FormLabel>
+                      <p>Enter your email address and we will send you further instructions</p>
+                    </FormLabel>
+                    <Button
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      size="medium"
+                      sx={buttonStyle}
+                      disabled={formik.isSubmitting}
+                    >
+                      Send Instructions
+                    </Button>
+                  </FormGroup>
+                  <div className={style.formFooter}>
+                    <div className={style.formEndnote}>Did you remember your password?</div>
+                    <NavLink
+                      to={PATH.LOGIN}
+                      className={({ isActive }) =>
+                        isActive ? style.activeLink + style.loginLink : style.loginLink
+                      }
+                    >
+                      Try logging in
+                    </NavLink>
+                  </div>
+                </FormControl>
+              </Paper>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  )
+})
+
+export const CheckEmail: FC<{ email: string }> = memo(({ email }) => {
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    redirect(PATH.LOGIN)
+  }
+
+  return (
     <div className={style.formBlock}>
       <div className={style.formContainer}>
-        <form onSubmit={formik.handleSubmit}>
-          <Paper elevation={6} sx={{ py: '40px', px: '30px', maxWidth: '413px' }}>
-            <FormControl fullWidth>
-              <div className={style.formHeader}>Forgot your password?</div>
-              <FormGroup>
-                <TextField
-                  id="email"
-                  type="text"
-                  variant="standard"
-                  label="Email"
-                  {...formik.getFieldProps('email')}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={
-                    formik.touched.email && formik.errors.email ? formik.errors.email : ' '
-                  }
-                />
-                <FormLabel>
-                  <p>Enter your email address and we will send you further instructions</p>
-                </FormLabel>
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  size="medium"
-                  sx={buttonStyle}
-                >
-                  Send Instructions
-                </Button>
-              </FormGroup>
-              <div className={style.formFooter}>
-                <div className={style.formEndnote}>Did you remember your password?</div>
-                <NavLink
-                  to={PATH.LOGIN}
-                  className={({ isActive }) =>
-                    isActive ? style.activeLink + style.loginLink : style.loginLink
-                  }
-                >
-                  Try logging in
-                </NavLink>
-              </div>
-            </FormControl>
-          </Paper>
-        </form>
+        <Paper elevation={6} sx={paperStyle}>
+          <div className={style.formHeader}>Check Email</div>
+          <div>
+            {/*<img src="#" alt="inbox" />*/}
+            <EmailPicture />
+          </div>
+          <FormLabel>
+            <p>{`We’ve sent an Email with instructions to ${email}`}</p>
+          </FormLabel>
+          <Button
+            color="primary"
+            variant="contained"
+            size="medium"
+            sx={buttonStyle}
+            onClick={handleClick}
+          >
+            Back to login
+          </Button>
+        </Paper>
       </div>
     </div>
   )
-}
+})
 
 //types
 type PasswordRecoveryFormType = {
