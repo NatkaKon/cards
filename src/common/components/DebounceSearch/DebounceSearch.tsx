b7e76fb9b36a8cbdab3b1446cc5eba15ad5dbd29
m1rade <forx98@gmail.com>
2023-03-17 01:00:54 +0300

    refactor(DebounceSearch): now updated with Packs State, then optimized

--- src/common/components/DebounceSearch/DebounceSearch.tsx
+++ src/common/components/DebounceSearch/DebounceSearch.tsx
@@ -1,18 +1,29 @@
-import React, { ChangeEvent, FC, memo, useCallback, useEffect, useState } from 'react'
+import React, { ChangeEvent, FC, memo, useCallback, useEffect, useRef, useState } from 'react'
 
 import { useAppDispatch } from '../../../app/store'
 import { searchPacksByNameAC } from '../../../features/Packs/packsReducer'
 import { useDebounce } from '../../../hooks/useDebounce'
 import SuperInputText from '../SuperInputText/SuperInputText'
 
-export const DebounceSearch: FC<{ searchQuery?: string }> = memo(props => {
+export const DebounceSearch: FC<{ searchQuery: string }> = memo(props => {
+  let isFirstRender = useRef(true)
+
   const dispatch = useAppDispatch()
-  const [searchQuery, setSearchQuery] = useState(props.searchQuery || '')
+  const [searchQuery, setSearchQuery] = useState('')
   const debouncedValue = useDebounce(searchQuery, 1000)
 
   useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false
+
+      return
+    }
     dispatch(searchPacksByNameAC(debouncedValue))
   }, [debouncedValue])
+
+  useEffect(() => {
+    setSearchQuery(props.searchQuery)
+  }, [props.searchQuery])
 
   const handleOnInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.currentTarget.value)
